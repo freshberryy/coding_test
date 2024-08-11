adf483c1ad79918c2da73aaaf4d3a9f610e2a40e
freshberryy <161305732+freshberryy@users.noreply.github.com>
2024-08-11 20:38:56 +0900

    c

--- a.cpp
+++ a.cpp
@@ -1,11 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-string s;
-int cnt[26];
-int main(){
-    cin >> s;
-    for(char i : s){
-        cnt[i - 'a']++;
+int const dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
+int n, a[104][104], visited[104][104], ret = 1, cnt;
+void dfs(int y, int x, int d){
+    visited[y][x] = 1;
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || nx < 0 || ny >= n || nx >= n)continue;
+        if(!visited[ny][nx] && a[ny][nx] > d)dfs(ny, nx, d);
     }
-    for(int i = 0; i < 26; i++) cout << cnt[i] << " ";
+}
+
+int main(){
+    cin >> n; 
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> a[i][j];
+        }
+    }
+    for(int d = 1; d < 101; d++){
+        memset(visited, 0, sizeof(visited));
+        cnt = 0;
+        for(int j = 0; j < n; j++){
+            for(int h = 0; h < n; h++){
+                if(a[j][h] > d && !visited[j][h]){
+                    dfs(j, h, d);
+                    cnt++;
+                }
+            }
+        }
+        ret = max(ret, cnt);
+    }
+    cout << ret;
 }
